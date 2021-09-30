6f9203d8709c1936f3dd11804d815ded6785bf59
Abhishek Dev <abhishek44dev@gmail.com>
2021-09-30 22:55:49 +0530

    Checkpoint 1: New Code Added - New logic is for parsing meaningful data from the docx files - Also updated the requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from scrapper_excercise import navigate_to_main_webpage, scrap_and_download_files
+from doc2html_scrapper import navigate_to_main_webpage, scrap_and_download_files, dataframe_to_xml
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     base_url = "https://www.courts.ca.gov"
     sub_url = "/opinions-slip.htm"
     query = "?Courts=X"
-    file_extension = ".doc"
-    soup_obj = navigate_to_main_webpage(base_url, sub_url, query)
-    scrap_and_download_files(soup_obj, base_url, file_extension)
+    file_extension = ".docx"
+    soup = navigate_to_main_webpage(base_url, sub_url)
+    new_dataset_dict = scrap_and_download_files(soup, base_url, file_extension)
+    dataframe_to_xml(new_dataset_dict)
